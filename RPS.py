daeb7b9f99e177a2e8323c4830a2593280008078
Tyler-Gallimore <lifforn@mailfence.com>
2023-05-31 00:52:04 +0000

    Made the game into a loop

--- RPS.py
+++ RPS.py
@@ -1,27 +1,30 @@
 import random
 
-user_input = input("Choose Rock, Paper, or Scissors: ")
-computer_input = random.randint(0, 2)
-if user_input == "Rock":
-    if computer_input == 0:
-        print("You Tied")
-    elif computer_input == 1:
-        print("You Lose")
-    elif computer_input == 2:
-        print("You Win")
-elif user_input == "Paper":
-    if computer_input == 0:
-        print("You Win")
-    elif computer_input == 1:
-        print("You Tied")
-    elif computer_input == 2:
-        print("You Lose")
-elif user_input == "Scissors":
-    if computer_input == 0:
-        print("You Lose")
-    elif computer_input == 1:
-        print("You Win")
-    elif computer_input == 2:
-        print("You Tied")
-else:
-    print("Wrong input")
+while True:
+    user_input = input("Choose Rock, Paper, Scissors or q to Quit: ")
+    computer_input = random.randint(0, 2)
+    if user_input == "Rock":
+        if computer_input == 0:
+            print("You Tied")
+        elif computer_input == 1:
+            print("You Lose")
+        elif computer_input == 2:
+            print("You Win")
+    elif user_input == "Paper":
+        if computer_input == 0:
+            print("You Win")
+        elif computer_input == 1:
+            print("You Tied")
+        elif computer_input == 2:
+            print("You Lose")
+    elif user_input == "Scissors":
+        if computer_input == 0:
+            print("You Lose")
+        elif computer_input == 1:
+            print("You Win")
+        elif computer_input == 2:
+            print("You Tied")
+    elif user_input == "q":
+        exit()
+    else:
+        print("Wrong input")
