096d698dc05dcb8b38defc2234b6456ec51525b5
Tyler-Gallimore <129459190+Tyler-Gallimore@users.noreply.github.com>
2023-06-14 21:04:13 -0400

    Update lab_6_step_2_loops.py

--- AWS Labs/lab_6_step_2_loops.py
+++ AWS Labs/lab_6_step_2_loops.py
@@ -36,7 +36,25 @@ def translate_loop():
 
 # Main Function - use to call other functions
 def main():
+    new_input_text_list()
     translate_loop()
+    new_list_comprehension()
+
+
 
 if __name__ == "__main__":
     main()
+
+# Create a list of the input text
+def new_input_text_list():
+    input_text = open_input()
+    new_list = []
+    for item in input_text:
+        text = item['Text']
+        new_list.append(text)
+    print(new_list)
+  
+def new_list_comprehension():
+    input_text = open_input()
+    list_comprehension = [item['Text'] for item in input_text]
+    print(list_comprehension)
